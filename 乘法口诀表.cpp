e869efef437f7306091b7e88ca153b7c9fcf5fe9
xzq <1040175504@qq.com>
2015-09-21 00:00:45 +0800

    Create 乘法口诀表.cpp

---
+++ 乘法口诀表.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y,z;
+	for(x=1;x<=9;x++) 
+	{
+	 for(y=1;y<=9;y++)
+	 {
+	 	cout<<x<<"*"<<y<<"="<<x*y<<" ";
+	  } 
+	  cout<<endl;
+	} 
+	return 0;
+}
+
