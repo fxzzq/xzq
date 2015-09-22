39af99b6e540b007cc95e6c51377c1c2ee11a937
xzq <1040175504@qq.com>
2015-09-22 13:32:15 +0800

    Create 猴子吃桃.cpp

---
+++ 猴子吃桃.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int i, peach;
+ 
+ peach=1;
+cin>>i;
+ while(i>1)
+ {
+  i--;
+  peach=(peach+1)*2;
+ }
+cout<<peach<<endl;
+return 0;
+} 
