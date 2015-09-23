04787359e59fa57ec189cb2c6bd64df4a64d6485
xzq <1040175504@qq.com>
2015-09-23 23:43:29 +0800

    Create 杨辉三角程序2.cpp

---
+++ 杨辉三角程序2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,i,j;
+int a[11][11];
+cout<<"请输入一个数(3-10)："<<endl;
+cin>>n;
+if(n<3||n>10)
+{
+cout<<"输入错误！请重新输入！"<<endl;
+return main();
+}
+else
+{
+for(i=1;i<11;i++)
+	for(j=1;j<=i;j++)
+       if(j==1||j==i)
+		   a[i][j]=1;
+       else
+		   a[i][j]=a[i-1][j-1]+a[i-1][j];
+
+for(i=1;i<11;i++)
+{
+	for(j=1;j<=i;j++)
+	{
+		cout<<"  "<<a[i][j];
+    }
+	cout<<endl;
+}
+}	
+	return 0;
+}
+
