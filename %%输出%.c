62f11870d95462c3fd89268c8d227cd0c198ce3b
xzq <1040175504@qq.com>
2015-09-24 22:30:35 +0800

    Update %%输出%.c

--- %%输出%.c
+++ %%输出%.c
@@ -8,6 +8,6 @@ int main()
 	  printf("%d-%d=%d\n",a,b,a-b);
 	  printf("%d*%d=%d\n",a,b,a*b);
 	  printf("%d/%d=%d\n",a,b,a/b);
-      printf("%d%%%d=%d\n",a,b,a%b);
+          printf("%d%%%d=%d\n",a,b,a%b);
     return 0;
 }
