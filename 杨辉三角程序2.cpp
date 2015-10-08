cd127b7cca60b266a1c83ce1914abbc2d9aab466
xzq <1040175504@qq.com>
2015-10-08 22:58:08 +0800

    Update 杨辉三角程序2.cpp

--- 杨辉三角程序2.cpp
+++ 杨辉三角程序2.cpp
@@ -13,14 +13,14 @@ return main();
 }
 else
 {
-for(i=1;i<11;i++)
+for(i=1;i<=n;i++)
 	for(j=1;j<=i;j++)
        if(j==1||j==i)
 		   a[i][j]=1;
        else
 		   a[i][j]=a[i-1][j-1]+a[i-1][j];
 
-for(i=1;i<11;i++)
+for(i=1;i<=n;i++)
 {
 	for(j=1;j<=i;j++)
 	{
