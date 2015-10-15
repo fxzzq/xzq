b85101635083594278864b22bb0959e58cd7b69e
xzq <1040175504@qq.com>
2015-10-15 19:27:44 +0800

    Create 递归字符串反序2.cpp

---
+++ 递归字符串反序2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+void reverse( string& s)
+{        
+	int start=0,end=s.length();
+	string tem(s);
+while(start<end)
+{
+	end--;
+	swap(tem[start],tem[end]);
+	start++;
+}
+	s=tem;
+	cout<<s;
+}
+int main()
+{
+string str;
+cout<<"please input a string:";
+getline(cin,str);
+reverse(str);
+cout<<endl;
+return 0;
+}
