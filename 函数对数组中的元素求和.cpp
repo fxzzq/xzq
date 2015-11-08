f28064a4403366efb0de77e3f66b211d50fe6e36
xzq <1040175504@qq.com>
2015-11-08 20:07:29 +0800

    Create 函数对数组中的元素求和.cpp

---
+++ 函数对数组中的元素求和.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+void add(int n, int *sum)
+{
+    for (int i = 0; i < n; i++)
+    {
+        sum[i] = 0;
+    }
+}
+int main()
+{
+    int a[20];
+    int i = 0,j=0;
+    while (j<=100)
+    {
+        cin >> a[i];
+        if (a[i] == -1)
+            break;
+        i++;
+        j++;
+	
+    }
+	int n=0;
+    int *sum = &a[0];//指针变量初始化
+    add(n,sum);
+    i = 0;
+    for (int k = 0; k < j;k++)
+    {
+      n+=a[k];
+    }
+	cout<<n;
+	cout<<endl;
+    return 0;
+}
